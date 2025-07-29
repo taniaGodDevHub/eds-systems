6d9c035994821a32ea3948c9db188e5e26e69d57
Sky <sky@sky.sky>
2025-07-29 11:14:12 +0300

    Настройка ТГ

--- controllers/TgController.php
+++ controllers/TgController.php
@@ -7,6 +7,19 @@ use Yii;
 
 class TgController extends AccessController
 {
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['Authorization'],
+                ],
+            ],
+        ];
+    }
     public function actionIndex()
     {
         Yii::info("Вебхук от ТG" . print_r($this->request->get(), true), 'tg');
