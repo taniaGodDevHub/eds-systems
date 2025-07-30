8998f18798cf13b53860a4b20bd93d3d1279f2e7
Sky <sky@sky.sky>
2025-07-30 11:52:06 +0300

    Настройка ТГ

--- controllers/TgController.php
+++ controllers/TgController.php
@@ -82,10 +82,7 @@ class TgController extends AccessController
             ->where(['chat_id' => $this->chat_id])
             ->with('managerProfile')
             ->one();
-        $this->telegram->sendMessage([
-            'chat_id' => $this->chat_id,
-            'text' => "Найденый менеджер" . print_r($issetManager, true)
-        ]);
+
         if(!empty($issetManager)){
             $this->telegram->sendMessage([
                 'chat_id' => $this->chat_id,
