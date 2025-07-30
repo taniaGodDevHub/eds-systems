427f1d70ce142e9c08845190f4599a25eeb654fa
Sky <sky@sky.sky>
2025-07-30 16:43:53 +0300

    Настройка ТГ

---
+++ models/TgMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tg_message".
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property int $message_id
+ * @property int $chat_id
+ */
+class TgMessage extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tg_message';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author_id', 'message_id', 'chat_id'], 'required'],
+            [['author_id', 'message_id', 'chat_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'author_id' => 'Author ID',
+            'message_id' => 'Message ID',
+            'chat_id' => 'Chat ID',
+        ];
+    }
+
+}
