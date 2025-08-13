35cecc9d378c4cdda68a6c16bfed366fc3973101
Sky <sky@sky.sky>
2025-08-13 23:12:38 +0300

    Внутренний чат с клиентами

--- config/web.php
+++ config/web.php
@@ -93,7 +93,7 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][] = 'prod';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
