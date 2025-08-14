268e81a21ff6286455c00b84fd7d2dd2b6f4ffdb
Sky <sky@sky.sky>
2025-08-14 22:49:20 +0300

    Внутренний чат с клиентами

--- migrations/m250814_183850_update_chat_message_table.php
+++ migrations/m250814_183850_update_chat_message_table.php
@@ -9,7 +9,10 @@ class m250814_183850_update_chat_message_table extends Migration
      */
     public function safeUp()
     {
-        $this->addColumn('chat_message', 'user_chat_id', $this->integer()->null());
+        $this->addColumn('chat_message', 'user_chat_id', $this->bigInteger()->null());
+        $this->alterColumn('chat_message', 'chat_id', $this->bigInteger()->null());
+        $this->alterColumn('manager_to_chat', 'chat_id', $this->bigInteger()->null());
+        $this->alterColumn('manager_to_chat', 'client_id', $this->bigInteger()->null());
     }
 
     /**
@@ -17,6 +20,6 @@ class m250814_183850_update_chat_message_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropColumn('chat_message', 'user_chat_id');
+        return false;
     }
 }
