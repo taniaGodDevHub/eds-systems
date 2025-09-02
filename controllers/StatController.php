372823c9e8b059a275986e858b217889db07e56a
Sky <sky@sky.sky>
2025-09-02 17:28:20 +0300

    stat

--- controllers/StatController.php
+++ controllers/StatController.php
@@ -87,7 +87,7 @@ class StatController extends AccessController
 
             $d->full_activity = 0;
             foreach ($currentActivity as $c) {
-                echo "$c->end_date $c->start_date: " . ($c->end_date - $c->start_date). "<br>";
+                //echo "$c->end_date $c->start_date: " . ($c->end_date - $c->start_date). "<br>";
                 $d->full_activity += $c->end_date - $c->start_date;
             }
 
